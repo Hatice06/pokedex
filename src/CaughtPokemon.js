1d478c21db10b1eec402c5e9c16ba66ab25502bd
Hatice06 <haticeaydogan00@gmail.com>
2022-04-04 18:57:18 +0100

    Exercise G works

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -3,13 +3,22 @@ import React, { useState } from "react";
 const CaughtPokemon = (props) => {
   // const [caught, setCaught] = useState(0);
   const [caught, setCaught] = useState([]);
-  //console.log(caught)
+  // console.log(caught);
+
+  const [pokemonNameInput, setPokemonNameInput] = useState("");
+  const handleInputChange = (event) => {
+    setPokemonNameInput(event.target.value);
+    // console.log(event.target.value);
+  };
   const catchPokemon = () => {
     // const newArray = caught.concat(ourPokemon);
     // setCaught(caught + 1);
-   setCaught(caught.concat("Pikachu"));
-   // const newArray = [...caught];
-   
+    // setCaught(caught.concat(pokemonNameInput));
+    // const newArray = [...caught];
+    if (pokemonNameInput !== "") {
+      setCaught(caught.concat(pokemonNameInput));
+    }
+    setPokemonNameInput("");
   };
 
   return (
@@ -17,19 +26,20 @@ const CaughtPokemon = (props) => {
       <p>
         Caught {caught.length} Pokemon on {props.date}
       </p>
-      { <ul>
-        {caught.map((pokedex, index) => {
-          return <li key={index}>{pokedex}</li>;
-        })}
-      </ul>}
-
+      {
+        <ul>
+          {caught.map((pokedex, index) => {
+            return <li key={index}>{pokedex}</li>;
+          })}
+        </ul>
+      }
+      <input
+        type="text"
+        value={pokemonNameInput}
+        onChange={handleInputChange}
+      ></input>
       <button onClick={catchPokemon}>Click me!</button>
     </div>
   );
 };
 export default CaughtPokemon;
-
-
-
-
-
